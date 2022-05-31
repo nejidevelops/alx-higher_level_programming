ed0ce2d925c873f3c0f396706e4f2228caf29628
NewtonOmbese <newtonombese1@gmail.com>
2022-05-31 23:02:24 +0300

    removing char task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    copy = ''
+    if len(str) == 0:
+        return str
+    for i in range(len(str)):
+        if i != n:
+            copy += str[i]
+    return copy
