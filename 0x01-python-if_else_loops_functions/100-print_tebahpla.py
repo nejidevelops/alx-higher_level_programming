187b7325c43b008a2fcd5df42ea4a5980b37dba9
NewtonOmbese <newtonombese1@gmail.com>
2022-05-31 22:57:05 +0300

    print tebahplay file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-i = 0
-for j in range(ord('z'), ord(a) -1, -1):
-    print("{}".format(chr(j - i)), end="")
-    i = 32 if i == 0 else 0
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flag == 1:
+        i = i + 32
+    i = i - 1
+    
