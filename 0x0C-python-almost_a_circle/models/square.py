e639c68d7679865f34319ac236e04d6b52b7e421
NewtonOmbese <newtonombese1@gmail.com>
2022-07-11 20:14:27 +0300

    square py file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Creating a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defining square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializing square class"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Printing square class by overloading __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+
+    @property
+    def size(self):
+        """Getting size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setting size of square"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updating square class"""
+        if args:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Returning dictionary representation of square"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
