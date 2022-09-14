81366f17f1e1a8ef410432dcf156ae730f138c2d
Newton Ombese <newtonombese1@gmail.com>
2022-09-14 22:33:57 +0300

    model city files for number 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""prints all City objects
-from the database hbtn_0e_14_usa"""
+"""script to list all state objects using sqlalchemy
+"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
+import sys
 
-    import sys
-    from model_state import Base, State
-    from model_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
 
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                              .filter(City.state_id == State.id)\
-                              .order_by(City.id).all():
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    for result in session.query(State.name, City.id, City.name)\
+            .join(City, City.state_id == State.id)\
+            .order_by(City.id):
+        print("{}: ({}) {}".format(result[0], result[1], result[2]))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Definition of the City class"""
-
-from sqlalchemy.ext.declarative import declarative_base
+"""script for using sqlalchemy to model our models using ORM
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
-    """Class City"""
-
+    """cities class for use with sqlalchemy
+        -> inherits from sqlalchemy declarative_base
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
