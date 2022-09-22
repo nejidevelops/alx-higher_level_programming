000567f65d5c80002d588c84a79fb4baebb44662
Newton Ombese <newtonombese1@gmail.com>
2022-09-22 23:22:00 +0300

    header curl

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to URL displays the body of the response
-curl -s -H "X-School-User-Id: 98" $1
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
